3f96f0211129a3f2ffe7fe1b733bf6e05b5d81d8
Mohamad Dabboussi <dabboussi99@gmail.com>
2020-10-31 13:19:44 +0200

    change structure

--- backend/server/api/models.py
+++ backend/server/api/models.py
@@ -5,4 +5,4 @@ class Movie(models.Model):
     id = models.CharField(max_length=128, primary_key=True)
     imdbid = models.CharField(max_length=1024)
     title = models.CharField(max_length=265)
-    overview = models.CharField(max_length=256)
+    overview = models.TextField()
