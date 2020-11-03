4b7ece59f4d50f73bfa77c4cdbfabf048ca220f7
Mohamad Dabboussi <dabboussi99@gmail.com>
2020-11-03 07:49:12 +0200

    add response search

--- backend/server/api/responses_1.py
+++ backend/server/api/responses_1.py
@@ -5,7 +5,7 @@ import json
 
 def getBygenres(genre):
     movies_cleaned_df = pd.read_csv(
-       "/media/omar/My Files/Programming/WebApp cours/Project/Rainmaker-RecommendationSystem-Project/backend/Movies_Dataset.csv")
+        "/media/omar/My Files/Programming/WebApp cours/Project/Rainmaker-RecommendationSystem-Project/backend/Movies_Dataset.csv")
 
     res = movies_cleaned_df[movies_cleaned_df['genres'].str.contains(
         genre, na=False)]
@@ -16,7 +16,7 @@ def getBygenres(genre):
 
 def getByyear(year):
     movies_cleaned_df = pd.read_csv(
-       "/media/omar/My Files/Programming/WebApp cours/Project/Rainmaker-RecommendationSystem-Project/backend/Movies_Dataset.csv")
+        "/media/omar/My Files/Programming/WebApp cours/Project/Rainmaker-RecommendationSystem-Project/backend/Movies_Dataset.csv")
 
     res = movies_cleaned_df[movies_cleaned_df['release_date'].str.contains(
         year, na=False)]
@@ -27,7 +27,7 @@ def getByyear(year):
 
 def getByrating(rating):
     movies_cleaned_df = pd.read_csv(
-       "/media/omar/My Files/Programming/WebApp cours/Project/Rainmaker-RecommendationSystem-Project/backend/Movies_Dataset.csv")
+        "/media/omar/My Files/Programming/WebApp cours/Project/Rainmaker-RecommendationSystem-Project/backend/Movies_Dataset.csv")
 
     res = movies_cleaned_df[movies_cleaned_df['rating'].astype(
         str).str[0] == rating[0]]
@@ -38,7 +38,7 @@ def getByrating(rating):
 
 def getByVotes():
     movies_cleaned_df = pd.read_csv(
-       "/media/omar/My Files/Programming/WebApp cours/Project/Rainmaker-RecommendationSystem-Project/backend/Movies_Dataset.csv")
+        "/media/omar/My Files/Programming/WebApp cours/Project/Rainmaker-RecommendationSystem-Project/backend/Movies_Dataset.csv")
 
     movies_sortvote_df = movies_cleaned_df.sort_values(
         by=['rating', 'vote_count'], ascending=False)[movies_cleaned_df['vote_count'] > 100]
@@ -50,7 +50,7 @@ def getByVotes():
 
 def getByPopularity():
     movies_cleaned_df = pd.read_csv(
-       "/media/omar/My Files/Programming/WebApp cours/Project/Rainmaker-RecommendationSystem-Project/backend/Movies_Dataset.csv")
+        "/media/omar/My Files/Programming/WebApp cours/Project/Rainmaker-RecommendationSystem-Project/backend/Movies_Dataset.csv")
 
     movies_sortpopularity_df = movies_cleaned_df.sort_values(
         by=['popularity'], ascending=False)
@@ -62,12 +62,12 @@ def getByPopularity():
 
 def getById(id):
     movies_cleaned_df = pd.read_csv(
-       "/media/omar/My Files/Programming/WebApp cours/Project/Rainmaker-RecommendationSystem-Project/backend/Movies_Dataset.csv")
+        "/media/omar/My Files/Programming/WebApp cours/Project/Rainmaker-RecommendationSystem-Project/backend/Movies_Dataset.csv")
 
     print(id)
     i = int(str(id))
     res = movies_cleaned_df[movies_cleaned_df['id'] == i]
-    
+
     result = res.to_json(orient="records")
     parsed = json.loads(result)
     return parsed
@@ -75,7 +75,7 @@ def getById(id):
 
 def getBy_genres_year_rating(genre, year, rating):
     movies_cleaned_df = pd.read_csv(
-       "/media/omar/My Files/Programming/WebApp cours/Project/Rainmaker-RecommendationSystem-Project/backend/Movies_Dataset.csv")
+        "/media/omar/My Files/Programming/WebApp cours/Project/Rainmaker-RecommendationSystem-Project/backend/Movies_Dataset.csv")
 
     a = movies_cleaned_df[movies_cleaned_df['genres'].str.contains(
         genre, na=False)]
@@ -85,5 +85,16 @@ def getBy_genres_year_rating(genre, year, rating):
     parsed = json.loads(result)
     return parsed
 
+
+def getBy_genres_year_rating(genre, year, rating):
+    movies_cleaned_df = pd.read_csv(
+        "/media/omar/My Files/Programming/WebApp cours/Project/Rainmaker-RecommendationSystem-Project/backend/Movies_Dataset.csv")
+    movies_cleaned_df['description'] = movies_cleaned_df['description'].str.lower()
+    title = title.lower()
+    res = movies_cleaned_df[movies_cleaned_df['description'].str.contains(
+        title, na=False)]
+    result = res.to_json(orient="records")
+    parsed = json.loads(result)
+    return parsed
 
 # movies_cleaned_df.original_language.unique()
